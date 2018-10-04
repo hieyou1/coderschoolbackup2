5e9d7347cd3dfa6fb50255c0852650dd7913528c
hieyou1 <mgevarts@outlook.com>
2018-10-04 17:24:19 -0500

    Commited automatically using pushgit.bat

--- jsfile/index.js
+++ jsfile/index.js
@@ -1,8 +1,12 @@
 const fs = require('fs');
 const ejs = require('ejs');
 const express = require('express');
+const bodypars = require('body-parser');
 const app = express();
+app.use(bodypars.urlencoded({ extended: false}));
+app.use(bodypars.json());
 const port = 9536;
+const currentPath = __dirname;
 function readFile(file) {
   var out = fs.readFile(file, function (err, data) {
     if (err) {return "error";}
@@ -14,7 +18,17 @@ function readFile(file) {
     return out;
   }
 }
-app.get('/', function (req, res) {
-
+function filePassUpload(rawfile, filename) {
+  var file = new Uint8Array(Buffer.from(rawfile));
+  fs.writeFile("data/" + filename, file, function (err) {if (err) throw err;});
+}
+app.get('/upload', function (req, res) {
+  res.sendFile(currentPath + '/pub/upload.html');
 });
-app.listen(port, () >= console.log("Listening on port " + port));
+app.post('/upload', function (req, res) {
+  file = req.body.supboi;
+  filename = file.filename;
+  console.log(req.body);
+  res.sendFile(currentPath + "/pub/upload.html");
+});
+app.listen(port, () => console.log("Listening on port " + port));
