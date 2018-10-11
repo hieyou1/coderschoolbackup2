9f34acbca3f089f77073886b59540d2585f375fd
hieyou1 <mgevarts@outlook.com>
2018-10-11 17:26:27 -0500

    Commited automatically using pushgit.bat

--- jsfile/index.js
+++ jsfile/index.js
@@ -1,14 +1,15 @@
 const fs = require('fs');
-const ejs = require('ejs');
+//const ejs = require('ejs');
 const express = require('express');
 const bodypars = require('body-parser');
 const app = express();
+app.use((thisisanunusedvariable, res, next) => {res.header("Access-Control-Allow-Origin", "*");next();});
 app.use(bodypars.urlencoded({ extended: false}));
 app.use(bodypars.json());
 const port = 9536;
 const currentPath = __dirname;
 function readFile(file) {
-  var out = fs.readFile(file, function (err, data) {
+  var out = fs.readFile(file, (err, data) => {
     if (err) {return "error";}
     return data;
   });
@@ -20,15 +21,18 @@ function readFile(file) {
 }
 function filePassUpload(rawfile, filename) {
   var file = new Uint8Array(Buffer.from(rawfile));
-  fs.writeFile("data/" + filename, file, function (err) {if (err) throw err;});
+  fs.writeFile("data/" + filename, file, (err) => {if (err) throw err;});
 }
-app.get('/upload', function (req, res) {
+app.get('/upload', (req, res) => {
   res.sendFile(currentPath + '/pub/upload.html');
 });
-app.post('/upload', function (req, res) {
-  file = req.body.supboi;
-  filename = file.filename;
-  console.log(req.body);
+app.post('/upload', (req, res) => {
+  // file = req.body.supboi;
+  var b64raw = req.body.file;
+  var b64 = b64raw.substring(b64raw.indexOf("base64,") + 7, b64raw.length);
+  var filename = req.body.filename.substring(req.body.filename.indexOf('fakepath\\') + 9, req.body.filename.length);
+  console.log(`Uploaded file: ${filename}`);
+  fs.writeFile(`data/${filename}`, b64, 'base64', (err) => {if (err) console.log(`err: ${err}`);});
   res.sendFile(currentPath + "/pub/upload.html");
 });
 app.listen(port, () => console.log("Listening on port " + port));
