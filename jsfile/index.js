4c06d1bd4504488a26f6a0bc216d4ed528410c7a
hieyou1 <mgevarts@outlook.com>
2018-10-25 17:28:48 -0500

    Commited automatically using pushgit.bat

--- jsfile/index.js
+++ jsfile/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodypars = require('body-parser');
 const { join } = require('path');
 const app = express();
-app.use((thisisanunusedvariable, res, next) => {res.header("Access-Control-Allow-Origin", "*"); next();});
+app.use((thisisanunusedvariable, res, next) => { res.header("Access-Control-Allow-Origin", "*"); next(); });
 app.use(bodypars.urlencoded({ extended: false }));
 app.use(bodypars.json());
 const port = 9536;
@@ -12,7 +12,7 @@ const currentPath = __dirname;
 app.use("/data", express.static(currentPath + "/data"));
 function readFile(file) {
   var out = fs.readFile(file, (err, data) => {
-    if (err) {return "error";}
+    if (err) { return "error"; }
     return data;
   });
   if (out == "error") {
@@ -23,7 +23,7 @@ function readFile(file) {
 }
 function filePassUpload(rawfile, filename) {
   var file = new Uint8Array(Buffer.from(rawfile));
-  fs.writeFile("data/" + filename, file, (err) => {if (err) throw err;});
+  fs.writeFile("data/" + filename, file, (err) => { if (err) throw err; });
 }
 app.get('/upload', (req, res) => {
   res.sendFile(currentPath + '/pub/upload.html');
@@ -31,16 +31,31 @@ app.get('/upload', (req, res) => {
 app.post('/upload', (req, res) => {
   // file = req.body.supboi;
   var b64raw = req.body.file;
-  var b64 = b64raw.substring(b64raw.indexOf("base64,") + 7, b64raw.length);
+  var b64;
+  if (req.body.type == "ethan") {
+    b64 = b64raw;
+  } else if (req.body.type == "file") {
+    b64 = b64raw.substring(b64raw.indexOf("base64,") + 7, b64raw.length);
+  }
   var filename = req.body.filename.substring(req.body.filename.indexOf('fakepath\\') + 9, req.body.filename.length);
   console.log(`Uploaded file: ${filename}`);
-  fs.writeFile(`data/${filename}`, b64, 'base64', (err) => {if (err) console.log(`err: ${err}`);});
+  if (req.body.type == "ethan") {
+    fs.writeFile(`data/${filename}.folder`, b64, 'base64', (err) => { if (err) console.log(`err: ${err}`); });
+  } else if (req.body.type == "file") {
+    fs.writeFile(`data/${filename}`, b64, 'base64', (err) => { if (err) console.log(`err: ${err}`); });
+  }
   res.sendFile(currentPath + "/pub/upload.html");
 });
 app.get('/download', (req, res) => {
   fs.readdir(currentPath + "/data", (err, files) => {
     if (err) console.log(`fserr: ${err}`);
-    ejs.renderFile("pub/download.ejs", {data : files}, (err2, data) => {
+    let folders = [];
+    for (file of files) {
+      if (file.indexOf(".folder") == file.length - 7) {
+        folders.push(file);
+      }
+    }
+    ejs.renderFile("pub/download.ejs", { data: files, folders: folders }, (err2, data) => {
       if (err2) console.log(`ejserr: ${err}`);
       console.log(`Data: ${data}`);
       res.send(data);
